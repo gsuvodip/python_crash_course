a5f2e5973488f8279e27bc7344a26bf1f1c16676
Suvodip Ghosh <suvodipghosh17@gmail.com>
2026-03-27 16:54:58 +0530

    ship movements - L and R

--- alien_invasion/ship.py
+++ alien_invasion/ship.py
@@ -11,5 +11,16 @@ class Ship():
         self.rect.centerx = self.screen_rect.centerx
         self.rect.bottom = self.screen_rect.bottom
 
+        # movement flags
+        self.moving_right = False
+        self.moving_left = False
+
+    def update(self):
+        """Update the ship's position based on movement flag"""
+        if self.moving_right:
+            self.rect.centerx += 1
+        if self.moving_left:
+            self.rect.centerx -= 1
+
     def blitme(self):
         self.screen.blit(self.image, self.rect)
