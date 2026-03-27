eaaff19e3f020b82300d868c2ebd7b9543d5301d
Suvodip Ghosh <suvodipghosh17@gmail.com>
2026-03-27 18:58:12 +0530

    adjusting ship's speed

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -13,7 +13,7 @@ def run_game():
     screen = pygame.display.set_mode((game_settings.screen_width, game_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
 
-    ship = Ship(screen)
+    ship = Ship(game_settings, screen)
 
     while True:
         gf.check_events(ship)
--- alien_invasion/ship.py
+++ alien_invasion/ship.py
@@ -1,8 +1,9 @@
 import pygame
 
 class Ship():
-    def __init__(self, screen):
+    def __init__(self, game_settings, screen):
         self.screen = screen
+        self.game_settings = game_settings
 
         self.image = pygame.image.load('images/ship.bmp')
         self.rect = self.image.get_rect()
@@ -11,16 +12,23 @@ class Ship():
         self.rect.centerx = self.screen_rect.centerx
         self.rect.bottom = self.screen_rect.bottom
 
+        # store a decimal value for ship's center
+        self.center = float(self.rect.centerx)
+
         # movement flags
         self.moving_right = False
         self.moving_left = False
 
     def update(self):
         """Update the ship's position based on movement flag"""
+        # update ship's center value not rect
         if self.moving_right:
-            self.rect.centerx += 1
+            self.center += self.game_settings.ship_speed_factor
         if self.moving_left:
-            self.rect.centerx -= 1
+            self.center -= self.game_settings.ship_speed_factor
+
+        # update rect object from self.center
+        self.rect.centerx = self.center
 
     def blitme(self):
         self.screen.blit(self.image, self.rect)
