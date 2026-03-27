f5cd779ad10f8e16394ee2b19b1d113779ebf074
Suvodip Ghosh <suvodipghosh17@gmail.com>
2026-03-27 19:00:33 +0530

    limiting ship's range

--- alien_invasion/ship.py
+++ alien_invasion/ship.py
@@ -22,9 +22,9 @@ class Ship():
     def update(self):
         """Update the ship's position based on movement flag"""
         # update ship's center value not rect
-        if self.moving_right:
+        if self.moving_right and self.rect.right < self.screen_rect.right:
             self.center += self.game_settings.ship_speed_factor
-        if self.moving_left:
+        if self.moving_left and self.rect.left > 0:
             self.center -= self.game_settings.ship_speed_factor
 
         # update rect object from self.center
