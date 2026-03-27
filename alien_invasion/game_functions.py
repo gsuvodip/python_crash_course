8e2e7812a9f9230ac74ef379fc3f4f713089f564
Suvodip Ghosh <suvodipghosh17@gmail.com>
2026-03-27 19:04:14 +0530

    refactoring check_events()

--- alien_invasion/game_functions.py
+++ alien_invasion/game_functions.py
@@ -8,16 +8,23 @@ def check_events(ship):
         if event.type == pygame.QUIT:
             sys.exit()
         elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_RIGHT:
-                ship.moving_right = True
-            elif event.key == pygame.K_LEFT:
-                ship.moving_left = True
+            check_keydown_events(event, ship)
         elif event.type == pygame.KEYUP:
-            if event.key == pygame.K_RIGHT:
-                ship.moving_right = False
-            elif event.key == pygame.K_LEFT:
-                ship.moving_left = False
+            check_keyup_events(event, ship)
 
+def check_keydown_events(event, ship):
+    """respond to key presses"""
+    if event.key == pygame.K_RIGHT:
+        ship.moving_right = True
+    elif event.key == pygame.K_LEFT:
+        ship.moving_left = True
+
+def check_keyup_events(event, ship):
+    """respond to key releases"""
+    if event.key == pygame.K_RIGHT:
+        ship.moving_right = False
+    elif event.key == pygame.K_LEFT:
+        ship.moving_left = False
 
 def update_screen(game_settings, screen, ship):
     """Update images on the screen and flip to the new screen"""
